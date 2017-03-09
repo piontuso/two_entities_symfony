808db40383f2b0d7923d9861176cca37b33588c8
Piontuso <piontuso@gmail.com>
2017-03-09 18:03:57 +0100

    add event listener

--- src/Piontuso/TwoEntitiesBundle/Controller/DefaultController.php
+++ src/Piontuso/TwoEntitiesBundle/Controller/DefaultController.php
@@ -6,12 +6,20 @@ use Piontuso\TwoEntitiesBundle\Entity\Comments;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 class DefaultController extends Controller
 {
+    /**
+     * Default constant user identifier.
+     */
+    const USER_ID = 1;
+
     /**
      * @Route("/comment")
      */
@@ -20,10 +28,20 @@ class DefaultController extends Controller
         $comments = new Comments();
         $comments->setTitle('Comment title');
         $comments->setContent('Your comment ...');
+        // Event listener.
+        $event_listener = function (FormEvent $event) {
+          // Get comment data.
+          $comment = $event->getData();
+          // Get service.
+          $custom_mailer =  $this->get('two_entities.mailer');
+          $custom_mailer->sendmail($comment->getTitle(), $comment->getContent());
+        };
+        // Create form.
         $form = $this->createFormBuilder($comments)
           ->add('title', TextType::class)
-          ->add('content', TextType::class)
+          ->add('content', TextareaType::class)
           ->add('save', SubmitType::class, array('label' => 'Add Comment'))
+          ->addEventListener(FormEvents::POST_SUBMIT, $event_listener)
           ->getForm();
 
         $form->handleRequest($request);
@@ -32,7 +50,7 @@ class DefaultController extends Controller
           $em = $this->getDoctrine()->getManager();
           $user = $this->getDoctrine()
             ->getRepository('TwoEntitiesBundle:User')
-            ->find(1);
+            ->find(self::USER_ID);
           if (is_object($user)) {
             $comments->setUser($user);
             $em->persist($comments);
--- src/Piontuso/TwoEntitiesBundle/Service/UserMailer.php
+++ src/Piontuso/TwoEntitiesBundle/Service/UserMailer.php
@@ -8,34 +8,50 @@ namespace Piontuso\TwoEntitiesBundle\Service;
  */
 class UserMailer
 {
-  private $mailer;
+    /**
+     * @var object
+     */
+    private $mailer;
 
-  /**
-   * @param $mailer
-   */
-  public function __construct($mailer)
-  {
-    $this->mailer = $mailer;
-  }
-
-  /**
-   * Send email.
-   */
-  public function sendmail()
-  {
-    global $kernel;
-    $container = $kernel->getContainer();
-    $mail_from = $container->getParameter('mail_from');
-    $mail_to = $container->getParameter('mail_to');
-
-    if (!empty($mail_from) && !empty($mail_to)) {
-      $message = \Swift_Message::newInstance()
-        ->setSubject('New comment')
-        ->setFrom($mail_from)
-        ->setTo($mail_to)
-        ->setBody('Your comment was added with successfully !');
-
-      $this->mailer->send($message);
+    /**
+     * Construct.
+     *
+     * @param object $mailer
+     *   Injected mailer service.
+     */
+    public function __construct($mailer)
+    {
+        $this->mailer = $mailer;
     }
-  }
+
+    /**
+     * Send email.
+     *
+     * @param string $subject
+     *   Mail subject.
+     * @param string $body
+     *   Mail body.
+     *
+     * @return bool
+     */
+    public function sendmail($subject, $body)
+    {
+        global $kernel;
+        if (empty($subject) || empty($body)) {
+          return false;
+        }
+        $container = $kernel->getContainer();
+        $mail_from = $container->getParameter('mail_from');
+        $mail_to = $container->getParameter('mail_to');
+
+        if (!empty($mail_from) && !empty($mail_to)) {
+          $message = \Swift_Message::newInstance()
+            ->setSubject($subject)
+            ->setFrom($mail_from)
+            ->setTo($mail_to)
+            ->setBody($body);
+
+          $this->mailer->send($message);
+        }
+    }
 }
